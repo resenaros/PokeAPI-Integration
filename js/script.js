fd81bb476c18976a424fb625bfd7c959ecc83e3a
Ariel R <resendiz.ariel6@gmail.com>
2025-04-24 22:43:38 -0700

    first and only commit

---
+++ js/script.js
@@ -0,0 +1,81 @@
+// declaring 3 constants by getting id
+const input = document.getElementById("pokemonInput");
+const button = document.getElementById("searchBtn");
+const pokemonList = document.getElementById("pokemonList");
+
+// Trigger search on click
+button.addEventListener("click", searchPokemon);
+
+// Trigger search on Enter key
+input.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    searchPokemon();
+  }
+});
+
+async function searchPokemon() {
+  const query = input.value.trim().toLowerCase();
+
+  //   if query is empty and enter is clicked show this
+  if (!query) {
+    pokemonList.innerHTML = `<p class="text-warning">Please enter a Pokémon name or ID.</p>`;
+    return;
+  }
+
+  try {
+    // Shows loading message. Enter random strings like "gg" in search bar + enter or click to show and then will show an error message
+    pokemonList.innerHTML = `<p class="text-light">Loading...</p>`;
+
+    // fetching data from api passing the query variable as part of the url
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${query}`);
+    // error handling: this will show up on conolse in dev tools
+    if (!response.ok) throw new Error("Not found");
+
+    const data = await response.json();
+
+    // padstart only works with strings so we convert the input to a string then adjust the target length followed by what character we add if the string does'nt meet the required length
+    const id = data.id.toString().padStart(3, "0");
+    // official artwork has a - therefoe I am not able to use a dot notation so opted for brackets
+    const image = data.sprites.other["official-artwork"].front_default;
+
+    const types = data.types.map((t) => t.type.name);
+
+    // Update the DOM with the fetched Pokémon
+    pokemonList.innerHTML = `
+        <div class="position-relative text-uppercase bg-secondary text-warning border border-2 border-primary rounded-3 overflow-hidden" style="isolation: isolate">
+          <p class=" start-50 translate-middle-x position-absolute text-dark opacity-50" style="font-size: 8rem; z-index: -1">
+            #${id}
+          </p>
+          <div>
+            <img class="w-50 mx-auto d-block" src="${image}" alt="${
+      data.name
+    }" />
+          </div>
+          <div>
+            <div class="numberContainer text-dark d-flex flex-wrap justify-content-center gap-2">
+              <p class="pokemonID">#${id}</p>
+              <h2 class=" fs-3">${data.name}</h2>
+            </div>
+            <div class=" d-flex flex-wrap justify-content-center gap-2" style="font-size: 12px">
+              ${types
+                .map(
+                  (type) =>
+                    // added type.tolowercase because style sheet is case sensitive
+                    `<p class=" ${type.toLowerCase()} text-center rounded px-3 py-1">${type}</p>`
+                )
+                .join("")}
+            </div>
+          </div>
+        </div>
+      `;
+  } catch (error) {
+    pokemonList.innerHTML = `<p class="text-danger fw-bold">Pokémon not found. Please try again.</p>`;
+  }
+  //   clears search bar after query ex: search 4 or charmander and iot clears the search bar for new input
+  input.value = "";
+}
+
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
---
+++ readme.txt
@@ -0,0 +1,38 @@
+PokeAPI Assignment Summary
+In this assignment I utilized HTML, CSS, Bootstrap, and JavaScript to create a web application that interacts with the PokeAPI. The project involved the following key components:
+
+HTML Template: An HTML template was designed to structure the web page. Part of this template was imported into the JavaScript file to facilitate DOM manipulation.
+
+Custom CSS: A custom CSS file was created to style Pokémon types and other elements where Bootstrap alternatives were not available.
+
+Bootstrap: Bootstrap was used to enhance the layout and responsiveness.
+
+JavaScript: JavaScript was used to fetch data from the PokeAPI. The script retrieved Pokémon names, IDs, types, and images, and inject the data into the DOM to display this information.
+
+Comments placed to interpret code and cleaned up code to remove unused code/classes/ids/attributes
+
+Requirements Completed:
+
+Included a search input in the navbar, triggered by an event listener on either the search button click or Enter key press.
+
+Utilized the PokeAPI to fetch Pokémon data.
+
+Displayed the Pokémon's image, ID number, name, and types on the page.
+
+Added a loading message and error messages for both empty input or invalid entries.
+
+Tools Used:
+
+ChatGPT: Assisted with starter code, research, and grammar.
+
+https://youtu.be/drXkf_rytRs?si=WMzEbtgV3jnjoOB7-YouTube Walkthrough: Used as a reference and source of inspiration.
+
+Images sourced from Google and Bing searches.
+
+Bootstrap 5 Documentation: Referenced for styling and layout guidance.
+
+Reflection:
+
+Continued focus will be placed on improving CSS layout(Flexbox and Grid) usage, as well as reinforcing fundamental JavaScript concepts and revisiting them until they are ingrained.
+
+Progress was made in using the command line, organizing project files, and working more efficiently in VS Code—especially through keyboard shortcuts. I also began using the Live Preview extension as an alternative to Live Server. It streamlined development by displaying updates directly within VS Code, though it lacks access to browser dev tools.
